aaa7b3e397fdd93fd72dd681b93ca76e0ede1b97
RobbieHuang2002 <robbie.huang2002@gmail.com>
2022-11-02 15:38:47 -0400

    added new image to front page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -973,7 +973,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    cityImg: file(relativePath: { eq: "CIDEE.png" }) {
+    cityImg: file(relativePath: { eq: "Queens_Vector.png" }) {
       childImageSharp {
         fluid(quality: 80, maxWidth: 1500) {
           ...GatsbyImageSharpFluid
